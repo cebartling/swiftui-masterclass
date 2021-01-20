aa1191ccb5bf2a0d3485d879d28ecbbbdc6899bd
Christopher Bartling <chris.bartling@gmail.com>
2021-01-19 23:40:54 -0600

    Completed Section 2, lesson 13

---
+++ Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Christopher Bartling on 1/19/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fructus/Fructus/View/StartButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Christopher Bartling on 1/19/21.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+    }
+}
