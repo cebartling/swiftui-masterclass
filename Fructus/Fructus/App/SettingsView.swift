75b2800e19066ca233a5975717beafac4715d660
Christopher Bartling <chris.bartling@gmail.com>
2021-01-22 13:39:49 -0600

    Section 2, lesson 19 completed.

--- Fructus/Fructus/App/SettingsView.swift
+++ Fructus/Fructus/App/SettingsView.swift
@@ -15,7 +15,19 @@ struct SettingsView: View {
         NavigationView() {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(spacing: 20) {
-                    Text("foobar")
+                    GroupBox(label: SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")) {
+                        Divider().padding(.vertical, 4)
+                        HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10) {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            
+                            Text("Most fruits are naturally low in fat, sodium and calories. None have cholesterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins and much more.")
+                                .font(.footnote)
+                        }
+                    }
                 }
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
                 .navigationBarItems(
