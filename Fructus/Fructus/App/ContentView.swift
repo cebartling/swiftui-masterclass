d31c8aeec95371e3010c0ba75548c4f259a27f80
Christopher Bartling <chris.bartling@gmail.com>
2021-01-22 08:34:21 -0600

    Section 2, lesson 19 in progress: Wiring up settings sheet and activating with button

--- Fructus/Fructus/App/ContentView.swift
+++ Fructus/Fructus/App/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     
     var fruits: [Fruit] = fruitsData
     
+    @State private var isShowingSettings: Bool = false
+    
     var body: some View {
         NavigationView {
             List {
@@ -22,6 +24,16 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Fruits")
+            .navigationBarItems(
+                trailing: Button(action: {
+                    isShowingSettings = true
+                }) {
+                    Image(systemName: "slider.horizontal.3")
+                }
+            )
+            .sheet(isPresented: $isShowingSettings) {
+                SettingsView()
+            }
         }
     }
 }
---
+++ Fructus/Fructus/App/SettingsView.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsView.swift
+//  Fructus
+//
+//  Created by Christopher Bartling on 1/22/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        NavigationView() {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 20) {
+                    Text("foobar")
+                }
+                .navigationBarTitle(Text("Settings"), displayMode: .large)
+                .navigationBarItems(
+                    trailing: Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image(systemName: "xmark")
+                    }
+                )
+                .padding()
+            }
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 12 Pro")
+    }
+}
