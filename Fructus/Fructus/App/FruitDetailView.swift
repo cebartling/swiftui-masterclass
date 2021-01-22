54094c23eec4cf18effc3740e433cfe92a4283a2
Christopher Bartling <chris.bartling@gmail.com>
2021-01-21 23:37:05 -0600

    Fixed issue with fruit detail view horizontal padding of header image

--- Fructus/Fructus/App/FruitDetailView.swift
+++ Fructus/Fructus/App/FruitDetailView.swift
@@ -15,8 +15,9 @@ struct FruitDetailView: View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(alignment: .center, spacing: 20) {
+                    FruitHeaderView(fruit: fruit)
+
                     VStack(alignment: .leading, spacing: 20) {
-                        FruitHeaderView(fruit: fruit)
                         
                         Text(fruit.title)
                             .font(.largeTitle)
