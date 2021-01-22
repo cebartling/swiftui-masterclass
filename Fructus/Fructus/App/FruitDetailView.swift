13c455643b2191c01206423225579b99bded55d1
Christopher Bartling <chris.bartling@gmail.com>
2021-01-21 21:49:28 -0600

    Completed section 2, lesson 17: Fruit detail view

---
+++ Fructus/Fructus/App/FruitDetailView.swift
@@ -0,0 +1,57 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Christopher Bartling on 1/21/21.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    
+    var fruit: Fruit
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .center, spacing: 20) {
+                    VStack(alignment: .leading, spacing: 20) {
+                        FruitHeaderView(fruit: fruit)
+                        
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                    }
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                }
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
+            }
+            .edgesIgnoringSafeArea(.top)
+        }
+    }
+}
+
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit: fruitsData[0])
+            .previewDevice("iPhone 12 Pro")
+    }
+}
