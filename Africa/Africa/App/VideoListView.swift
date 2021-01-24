c2b55880b637b4d6d683c0e25f7537f905c2420f
Christopher Bartling <chris.bartling@gmail.com>
2021-01-24 10:02:25 -0600

    Section 3 - AFRICA, lesson 24: Completed

---
+++ Africa/Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Christopher Bartling on 1/24/21.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Video list")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
