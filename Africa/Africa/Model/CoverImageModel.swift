379b2cf94e46e3004d4f633527329fd5b92c4811
Christopher Bartling <chris.bartling@gmail.com>
2021-01-24 21:29:44 -0600

    Section 3 - AFRICA, lesson 25: Completed

---
+++ Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Christopher Bartling on 1/24/21.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
