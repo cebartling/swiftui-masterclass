4434773b5c46e95cd3118fa5d7d6c90c80e2a5a5
Christopher Bartling <chris.bartling@gmail.com>
2021-02-01 10:39:40 -0600

    Section 3 - AFRICA, lesson 34: In progress...

--- Africa/Africa/App/GalleryView.swift
+++ Africa/Africa/App/GalleryView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct GalleryView: View {
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    @State private var selectedAnimal: String = "lion"
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack {
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: { value in
+                        gridSwitch()
+                    })
+
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { animal in
+                        Image(animal.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = animal.image
+                            }
+                    }
+                }
+                .onAppear(perform: {
+                    gridSwitch()
+                })
+            }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         }
-        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/ .infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
         .background(MotionAnimationView())
     }
 }
