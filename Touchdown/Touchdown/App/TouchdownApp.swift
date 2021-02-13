cf0389532268745fd831265dfa0b2ddcfed25cea
Christopher Bartling <chris@mac-mini-2020.hsd1.mn.comcast.net>
2021-02-13 17:24:40 -0600

    Touchdown: Lesson 40: In progress...

---
+++ Touchdown/Touchdown/App/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Christopher Bartling on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
