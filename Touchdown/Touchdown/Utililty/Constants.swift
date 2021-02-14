19370a1b771654e7bf6d59710afc521cd6efc42f
Christopher Bartling <chris.bartling@gmail.com>
2021-02-13 23:44:43 -0600

    Touchdown: Lesson 40: Completed

---
+++ Touchdown/Touchdown/Utililty/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Touchdown
+//
+//  Created by Christopher Bartling on 2/13/21.
+//
+
+import SwiftUI
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
