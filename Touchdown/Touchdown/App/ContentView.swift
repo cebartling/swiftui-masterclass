81cac9add00b9562bcfc577cdfaed86670c59285
Christopher Bartling <chris@mac-mini-2020.hsd1.mn.comcast.net>
2021-02-13 23:43:51 -0600

    Touchdown: Lesson 40: Completed

--- Touchdown/Touchdown/App/ContentView.swift
+++ Touchdown/Touchdown/App/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        FooterView()
+            .padding(.horizontal)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro")
     }
 }
