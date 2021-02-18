2519dfd44af5f163cf820379cdb918ad2d44922d
Christopher Bartling <chris.bartling@gmail.com>
2021-02-17 21:18:51 -0600

    Touchdown: Lesson 43 complete

--- Touchdown/Touchdown/App/ContentView.swift
+++ Touchdown/Touchdown/App/ContentView.swift
@@ -18,10 +18,16 @@ struct ContentView: View {
                     .background(Color.white)
                     .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
 
-                Spacer()
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    FeaturedTabView()
+                        .padding(.vertical, 20)
+                    
+                    VStack(spacing: 0) {
+                        FooterView()
+                            .padding(.horizontal)
+                    }
+                })
 
-                FooterView()
-                    .padding(.horizontal)
             }
             .background(colorBackground.ignoresSafeArea(.all, edges: .all))
         }
---
+++ Touchdown/Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Christopher Bartling on 2/17/21.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
