1cad2cf7291d4c58d77add81b77bf7e98eb49a2f
Christopher Bartling <chris.bartling@gmail.com>
2021-02-19 20:46:55 -0600

    Touchdown: Lesson 46 completed

---
+++ Touchdown/Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Christopher Bartling on 2/19/21.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
