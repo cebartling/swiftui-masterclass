896042f1760fc4e64b305e20d2e74a6f1ec39e74
Christopher Bartling <chris.bartling@gmail.com>
2021-03-21 21:50:01 -0500

    DEVOTE: Lession 54 completed

---
+++ Devote/Devote/Utility/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Christopher Bartling on 3/21/21.
+//
+
+import SwiftUI
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
