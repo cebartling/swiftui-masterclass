a46b0b0ffe6a427b4f90c53c3b6698f12be3966e
Christopher Bartling <chris.bartling@gmail.com>
2021-04-17 12:17:56 -0500

    Devote: Lesson 60 completed

--- Devote/Devote/View/BlankView.swift
+++ Devote/Devote/View/BlankView.swift
@@ -8,19 +8,26 @@
 import SwiftUI
 
 struct BlankView: View {
+    
+    var backgroundColor: Color
+    var backgroundOpacity: Double
+    
     var body: some View {
         VStack {
             Spacer()
         }
         .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .center)
-        .background(Color.black)
-        .opacity(0.7)
+        .background(backgroundColor)
+        .opacity(backgroundOpacity)
+        .blendMode(.overlay)
         .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct BlankView_Previews: PreviewProvider {
     static var previews: some View {
-        BlankView()
+        BlankView(backgroundColor: Color.black, backgroundOpacity: 0.3)
+            .background(BackgroundImageView())
+            .background(backgroundGradient.ignoresSafeArea(.all))
     }
 }
--- Devote/Devote/View/ContentView.swift
+++ Devote/Devote/View/ContentView.swift
@@ -100,9 +100,14 @@ struct ContentView: View {
                     .padding(.vertical, 0)
                     .frame(maxWidth: 640)
                 }
+                .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5))
 
                 if showNewTaskItem {
-                    BlankView()
+                    BlankView(
+                        backgroundColor: isDarkMode ? Color.black : Color.gray,
+                        backgroundOpacity: isDarkMode ? 0.3 : 0.5)
                         .onTapGesture {
                             withAnimation {
                                 showNewTaskItem = false
@@ -116,7 +121,10 @@ struct ContentView: View {
             }
             .navigationBarTitle("Daily Tasks", displayMode: .large)
             .navigationBarHidden(true)
-            .background(BackgroundImageView())
+            .background(
+                BackgroundImageView()
+                    .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+            )
             .background(backgroundGradient.ignoresSafeArea(.all))
         }
         .navigationViewStyle(StackNavigationViewStyle())
